a5df5751e9e8518ed7ee03721c6746f3bca02cb6
Jake Wood <jake@jakewood.org>
2013-06-18 18:33:55 -0400

    Adding more details to the stats - what keywords come from what web sites

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 var http = require('http');
 
-var websites = ['http://news.msn.com/',
-	'http://www.reddit.com/r/apocalypse',
-	'http://www.cnn.com/WORLD',
-	'http://news.yahoo.com/',
-	'http://news.google.com/',
-	'http://www.imdb.com/movies-in-theaters/',
-	'http://news.google.com/news/feeds?pz=1&cf=all&ned=us&hl=en&topic=m&output=rss'];
+var websites = {
+	'MSN News': 'http://news.msn.com/',
+	'Reddit (Apocalypse)': 'http://www.reddit.com/r/apocalypse',
+	'CNN World News': 'http://www.cnn.com/WORLD',
+	'Yahoo News': 'http://news.yahoo.com/',
+	'Google News': 'http://news.google.com/',
+	'IMDb Movies in Theaters': 'http://www.imdb.com/movies-in-theaters/',
+	'Google News (Health)': 'http://news.google.com/news/feeds?pz=1&cf=all&ned=us&hl=en&topic=m&output=rss'
+};
 
 var disasters = {
 	'Natural Disaster': ['tornado', 'fire', 'hurricane', 'thunder', 'lightning', 'storm'],
@@ -17,37 +19,22 @@ var disasters = {
 	'Asteroid / Astrological Event': ['asteroid', 'black hole', 'wormhole', 'worm hole', 'blackhole']
 };
 
-var counter = {};
+var disaster_counter = {};
+var website_counter = {};
 
 app.get('/', function(req, res) {
 	res.redirect('/index.html');
 });
 
-var util = require('util'),
-	twitter = require('twitter');
-	
-var twit = new twitter({
-	consumer_key: "aFZmgf2ivP9ZCqNrmg2YTQ",
-	consumer_secret: 'pGzS4jRYLYryjB89p6cA93HBQiHkwFK3kFUBTe2sMc',
-	access_token_key: '43029380-mDuqzVmiCvXYCmWg01X53tWJUA4vH3lRmWLkgBHi8',
-	access_token_secret: 'RzVSxIxkxrxdHolrs1En73hyjVYdKZinKY1z0n5xia0'
-});
-
-app.get('/twitter', function(req, res) {
-/*	twit.search('nodejs', function(data) {
-		res.send(util.inspect(data));
-	});*/
-	twit.getUserTimeline(function(data) {
-		res.send(data);
-	});
-});
-
 function updateFromWebsite(index, callback) {
-	if (index >= websites.length) {
+	if (index >= Object.keys(websites).length) {
 		return callback();
 	}
 	
-	http.get(websites[index], function(http_res) {
+	var website_name = Object.keys(websites)[index];
+	website_counter[website_name] = [];
+	
+	http.get(websites[website_name], function(http_res) {
 		http_res.setEncoding('utf8');
 		
 		var content = '';
@@ -57,10 +44,14 @@ function updateFromWebsite(index, callback) {
 		http_res.on('end', function() {
 			for (var d in disasters) { // go through each disaster...
 				for (var k in disasters[d]) { // each keyword for each disaster...
-					if (typeof counter[d] == 'undefined' || counter[d] == null) {
-						counter[d] = 0;
+				
+					if (typeof disaster_counter[d] == 'undefined' || disaster_counter[d] == null) {
+						disaster_counter[d] = 0;
 					}
-					counter[d] += (content.match(new RegExp(disasters[d][k], 'g'))||[]).length;
+					
+					var keyword_count = (content.match(new RegExp(disasters[d][k], 'g'))||[]).length;
+					disaster_counter[d] += keyword_count;
+					website_counter[website_name].push({ disaster: d, keyword: disasters[d][k] });
 				}
 			}
 			updateFromWebsite(index + 1, callback);
@@ -69,7 +60,8 @@ function updateFromWebsite(index, callback) {
 }
 
 app.get('/update', function(req, res) {
-	counter = {};
+	disaster_counter = {};
+	website_counter = {};
 	updateFromWebsite(0, function() {
 		res.send(counter);
 	});
@@ -77,10 +69,14 @@ app.get('/update', function(req, res) {
 
 app.get('/data', function(req, res) {
 	var data = [];
-	for (var i in counter) {
-		data.push([i, counter[i]]);
+	for (var i in disaster_counter) {
+		data.push([i, disaster_counter[i]]);
 	}
 	res.send(data);
+});
+
+app.get('/details', function(req, res) {
+	res.send(website_counter);
 });
 
 app.use(express.static(__dirname + '/static'));
