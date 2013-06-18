ce88d4fe523ff0dc133cd789cec505ee5f3baf48
Jake Wood <jake@jakewood.org>
2013-06-18 17:50:55 -0400

    Non database driven prototype

--- server.js
+++ server.js
@@ -1,10 +1,85 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+
+var websites = ['http://news.msn.com/', 'http://www.reddit.com/r/apocalypse'];
+
+var disasters = {
+	'Natural Disaster': ['tornado', 'fire'],
+	'Aliens': ['alien', 'ufo', 'u.f.o.'],
+	'Zombies': ['disease', 'virus', 'infection'],
+	'Asteroid / Astrological Event': ['asteroid', 'black hole', 'wormhole', 'worm hole', 'blackhole']
+};
+
+var counter = {};
 
 app.get('/', function(req, res) {
 	res.redirect('/index.html');
 });
 
+var util = require('util'),
+	twitter = require('twitter');
+	
+var twit = new twitter({
+	consumer_key: "aFZmgf2ivP9ZCqNrmg2YTQ",
+	consumer_secret: 'pGzS4jRYLYryjB89p6cA93HBQiHkwFK3kFUBTe2sMc',
+	access_token_key: '43029380-mDuqzVmiCvXYCmWg01X53tWJUA4vH3lRmWLkgBHi8',
+	access_token_secret: 'RzVSxIxkxrxdHolrs1En73hyjVYdKZinKY1z0n5xia0'
+});
+
+app.get('/twitter', function(req, res) {
+/*	twit.search('nodejs', function(data) {
+		res.send(util.inspect(data));
+	});*/
+	twit.getUserTimeline(function(data) {
+		res.send(data);
+	});
+});
+
+function updateFromWebsite(index, callback) {
+	if (index >= websites.length) {
+		return callback();
+	}
+	
+	http.get(websites[index], function(http_res) {
+		http_res.setEncoding('utf8');
+		
+		var content = '';
+		http_res.on('data', function(chunk) {
+			content += chunk;
+		});
+		http_res.on('end', function() {
+			for (var d in disasters) { // go through each disaster...
+				for (var k in disasters[d]) { // each keyword for each disaster...
+					if (typeof counter[d] == 'undefined' || counter[d] == null) {
+						counter[d] = 0;
+					}
+					counter[d] += (content.match(new RegExp(disasters[d][k], 'g'))||[]).length;
+				}
+			}
+			updateFromWebsite(index + 1, callback);
+		});
+	});
+}
+
+app.get('/update', function(req, res) {
+	counter = {};
+	updateFromWebsite(0, function() {
+		res.send(counter);
+	});
+});
+
+app.get('/data', function(req, res) {
+	var data = [];
+	for (var i in counter) {
+		data.push([i, counter[i]]);
+	}
+	res.send(data);
+});
+
 app.use(express.static(__dirname + '/static'));
 
-app.listen(process.env.PORT || 8000);
+// Initial update
+updateFromWebsite(0, function() {
+	app.listen(process.env.PORT || 8000);
+})
